3152f4afd06d30c003bc22f2d672d791f716ccce
Govinda <upadhyagovinda52@gmail.com>
2025-08-12 15:27:51 +0600

    cd test

--- apps/web/app/chats/page.tsx
+++ apps/web/app/chats/page.tsx
@@ -6,6 +6,6 @@ import ClientPage from "./clientPage";
 export default function Page() {
   const { data: session, status } = useSession();
   if (status === "loading") return <p>Loading session...</p>;
-  if (!session) return <p>You are not signed in.</p>;
+  if (!session) return <p>You are not signed in. Please signin</p>;
   return <ClientPage user={session.user.name} />;
 }
