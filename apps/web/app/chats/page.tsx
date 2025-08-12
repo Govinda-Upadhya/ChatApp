31188bc954a3165915a92432cb54126233ea73d6
Govinda <upadhyagovinda52@gmail.com>
2025-08-12 17:54:56 +0600

    changes

--- apps/web/app/chats/page.tsx
+++ apps/web/app/chats/page.tsx
@@ -5,7 +5,7 @@ import ClientPage from "./clientPage";
 
 export default function Page() {
   const { data: session, status } = useSession();
-  if (status === "loading") return <p>Loading session...</p>;
+  if (status === "loading") return <p>Loading...</p>;
   if (!session) return <p>You are not signed in. Please signin</p>;
   return <ClientPage user={session.user.name} />;
 }
